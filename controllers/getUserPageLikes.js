f1a5909e452ee5514755262fdbece0e85188d561
sofiegraham <sofiegraham@hotmail.com>
2017-12-15 18:59:55 -0800

    Filter only page_like ids

--- controllers/getUserPageLikes.js
+++ controllers/getUserPageLikes.js
@@ -3,7 +3,7 @@ const { PageLike } = require('../database/models/index.js');
 const getUserPageLikes = (req, res, next) => {
   return PageLike.where({
     user_id: Number(req.params.id),
-  }).fetchAll()
+  }).fetchAll({ columns: ['page_id'] })
     .then((results) => {
       req.pageLikes = results.models;
       next();
