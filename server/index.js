deae4a0688306b28c9074d2748c1d3b86908c81e
sofiegraham <sofiegraham@hotmail.com>
2017-12-16 17:49:58 -0800

    Add new relic tracking

--- server/index.js
+++ server/index.js
@@ -1,14 +1,8 @@
-const apm = require('elastic-apm-node').start({
-  // Required app name (allowed characters:a-z, A-Z, 0-9, -, _, and space)
-  appName: 'content-microservice',
-  // Use if APM Server requires a token
-  //secretToken: '',
-  // Set custom APM Server URL (default: http://localhost:8200)
-  //serverUrl: ''
-});
+require('dotenv').config();
+require('newrelic');
 
 const app = require('./app.js');
-require('dotenv').config();
+
 
 const PORT = process.env.SERVER_PORT || 8080;
 
