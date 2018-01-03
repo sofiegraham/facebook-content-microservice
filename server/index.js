4273eb0b47269ba3de4de67c83b52cf29d3b1c1c
sofiegraham <sofiegraham@hotmail.com>
2018-01-02 17:20:04 -0800

    Remove node clusters

--- server/index.js
+++ server/index.js
@@ -3,15 +3,15 @@ require('newrelic');
 const app = require('./routes.js');
 const cluster = require('cluster');
 
-if (cluster.isMaster) {
-  const cpuCount = require('os').cpus().length;
+// if (cluster.isMaster) {
+//   const cpuCount = require('os').cpus().length;
 
-  for (let i = 0; i < cpuCount; i += 1) {
-    cluster.fork();
-  }
-} else {
+//   for (let i = 0; i < cpuCount; i += 1) {
+//     cluster.fork();
+//   }
+// } else {
 
   const PORT = process.env.SERVER_PORT || 8080;
 
   app.listen(PORT, console.log(`now listening on port ${PORT}`));
-}
+// }
