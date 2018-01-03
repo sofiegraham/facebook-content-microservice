7f0100bb4fa69541bb3caeb0a3d6ed41bd04520e
sofiegraham <sofiegraham@hotmail.com>
2018-01-02 18:55:18 -0800

    Hardcode docker test redis image

--- cache/index.js
+++ cache/index.js
@@ -1,7 +1,7 @@
 const redis = require('redis');
 require('dotenv').config();
 
-const client = redis.createClient({ port: process.env.REDIS_PORT, host: 'redis' });
+const client = redis.createClient({ port: process.env.REDIS_PORT, host: process.env.REDIS_HOST });
 
 client.on('connect', () => {
   console.log('Redis connected');
