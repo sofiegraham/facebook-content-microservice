78688636cd4cf155e5310943bdc1e48f93fec356
sofiegraham <sofiegraham@hotmail.com>
2018-01-02 17:29:18 -0800

    Add redis host and port variables

--- cache/index.js
+++ cache/index.js
@@ -1,6 +1,7 @@
 const redis = require('redis');
+require('dotenv').config();
 
-const client = redis.createClient();
+const client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
 
 client.on('connect', () => {
   console.log('Redis connected');
