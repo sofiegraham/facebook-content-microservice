ac9546feca439c048ed32ea031f97ee855602134
sofiegraham <sofiegraham@hotmail.com>
2018-01-02 16:57:01 -0800

    Add manual curl tests for all routes

--- test/curlTests.js
+++ test/curlTests.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 
 const url = `http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`;
 
+// TEST POST USER POST
 // axios.post(`${url}/users/33/posts`, {
 //   content: 'PUPPIES ARE GREAT',
 // })
@@ -14,7 +15,7 @@ const url = `http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`;
 //   console.log(`${url}/users/5/posts`);
 // });
 
-// TEST POST LIKES
+// TEST POST POST LIKES
 // axios.post(`${url}/posts/17542652/likes`, {
 //     user_id: 22,
 //   })
@@ -26,19 +27,20 @@ const url = `http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`;
 //     console.log(`${url}/posts/17542652/likes`);
 //   });
 
-// TEST PAGE LIKES
-axios.post(`${url}/pages/100/likes`, {
-  user_id: 22,
-})
-.then(function (response) {
-  console.log(response);
-})
-.catch(function (error) {
-  console.log(error);
-  console.log(`${url}/pages/100/likes`);
-});
+// TEST POST PAGE LIKES
+// axios.post(`${url}/pages/100/likes`, {
+//   user_id: 22,
+// })
+// .then(function (response) {
+//   console.log(response);
+// })
+// .catch(function (error) {
+//   console.log(error);
+//   console.log(`${url}/pages/100/likes`);
+// });
 
-// axios.get(`${url}/users/4000/page_likes`)
+// TEST GET USER FEED
+// axios.get(`${url}/users/6252/feed`)
 // .then(function (response) {
 //   console.log(response.data);
 // })
@@ -46,8 +48,8 @@ axios.post(`${url}/pages/100/likes`, {
 //   console.log(error);
 // });
 
-
-// axios.get(`${url}/users/5000/page_likes`)
+// TEST GET USER PAGE LIKES
+// axios.get(`${url}/users/255/page_likes`)
 // .then(function (response) {
 //   console.log(response.data);
 // })
@@ -55,8 +57,17 @@ axios.post(`${url}/pages/100/likes`, {
 //   console.log(error);
 // });
 
+// TEST GET USER DEMOGRAPHIC
+// axios.get(`${url}/users/722/demographic`)
+// .then(function (response) {
+//   console.log(response.data);
+// })
+// .catch(function (error) {
+//   console.log(error);
+// });
 
-// axios.get(`${url}/users/3/feed`)
+// TEST GET LITSCOLUMNSCHEMA
+// axios.get(`${url}/listtables`)
 // .then(function (response) {
 //   console.log(response.data);
 // })
